7b566eb3a09d3567b3a1d332606ec16c49a416f2
Karata <vabc3h@gmail.com>
2017-06-12 01:37:14 +0800

    Add match test method.

--- tests/tdd.rs
+++ tests/tdd.rs
@@ -2,5 +2,15 @@ extern crate rpcre;
 
 #[test]
 fn simple() {
-    assert!(rpcre::m("d1", "d12"));
+    run_match("d1", vec![ "d12", "d18" ], vec![ "d212", "d8" ]);
+}
+
+fn run_match(pattern: &'static str, positive_values: Vec<&'static str>, negative_values: Vec<&'static str>) {
+    for val in positive_values {
+        assert!(rpcre::m(pattern, val), format!("'{}' should match '{}'", pattern, val));
+    }
+
+    for val in &negative_values {
+        assert!(!rpcre::m(pattern, val), format!("'{}' should not match '{}'", pattern, val));
+    }
 }
